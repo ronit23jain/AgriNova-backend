76ba0b4b37538be20de4c145451ed30031d4ed28
ronit23jain <jainronit129@gmail.com>
2026-06-13 00:18:38 +0530

    Add geographies endpoint

--- backend/src/controllers/agmarknetController.ts
+++ backend/src/controllers/agmarknetController.ts
@@ -48,3 +48,55 @@ export const getGeographies = async (
     });
   }
 };
+
+export const getMarkets = async (req: Request, res: Response) => {
+  try {
+    const response = await axios.post(
+      "https://api.ceda.ashoka.edu.in/v1/agmarknet/markets",
+      
+      {
+        ...req.body,
+        indicator: "price",
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.AGMARKNET_API_KEY}`,
+          Accept: "application/json",
+        },
+      }
+    );
+    console.log("BODY =", req.body);
+console.log("RESPONSE =", response.data);
+
+    res.json(response.data);
+  } catch (error: any) {
+    console.log(error.response?.data);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getPrices = async (req: Request, res: Response) => {
+  try {
+    const response = await axios.post(
+      "https://api.ceda.ashoka.edu.in/v1/agmarknet/prices",
+      req.body,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.AGMARKNET_API_KEY}`,
+          Accept: "application/json",
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error: any) {
+    console.log(error.response?.data);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/src/routes/agmarknetRoute.ts
+++ backend/src/routes/agmarknetRoute.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 import {
     getCommodities,
     getGeographies,
+    getMarkets,
+    getPrices,
 } from "../controllers/agmarknetController";
 
 const router = Router();
 
 router.get("/commodities", getCommodities);
 router.get("/geographies", getGeographies);
+router.post("/markets", getMarkets);
+router.post("/prices", getPrices);
 export default router;
